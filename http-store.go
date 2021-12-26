032db01b619c8b6f5dadb5f03ed3be8db51f6bd4
Matts <vdpmatts@gmail.com>
2021-12-26 12:31:06 +0100

    port

--- http-store.go
+++ http-store.go
@@ -1,12 +1,16 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+)
 
 func Index(w http.ResponseWriter, req *http.Request) {
 	http.ServeFile(w, req, "view/index.html")
 }
 
 func main() {
-	http.HandleFunc("/", Index)
+	port := os.Getenv("PORT")
+	http.HandleFunc("/"+port, Index)
 	http.ListenAndServe(":80", nil)
 }
